4ebc7e82d2d365089ea54ac5b2540efd82077f25
Harshith Padigela <harshith2794@gmail.com>
2014-03-22 21:34:38 +0530

    Update parser.hpp

--- Assignment-3/parser.hpp
+++ Assignment-3/parser.hpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <string.h>
+#include <cstring>
+#include <string>
 #include <stack>
 #include <vector>
+#include <fstream>
+#include <sstream>
 #include <set>
+#include <iterator>
 #include <map>
 
 using namespace std;
@@ -15,16 +19,16 @@ class Parser
 public:
         map< string, set<string> > grammar;
 
-        map< string, set<string> > first;
+        map< string, set<string> > firstSet;
 
-		map< string, set<string> > follow;
+		map< string, set<string> > followSet;
 
 		map<string , map<string,string> > parsing_table;
 
 
 		void getGrammar(string);
 		void start();
-		void getFollowSet(string );
+		void getFollowSet(string);
 		void getFirstSet(string);
 		void createTable();
 
